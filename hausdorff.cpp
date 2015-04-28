573c20b72988af907882b4c5e72dac858b72d7a4
sdhuerta <steven.huerta@mines.sdsmt.edu>
2015-04-27 20:44:18 -0600

    some changes

--- hausdorff.cpp
+++ hausdorff.cpp
@@ -48,16 +48,12 @@ bool equal(Image& image1, Image& image2)
  * @author Zachary Pierson
  *
  * @par Description:
- * Image_Model Constructor - Constructs Image_Model object from Image
+ * euclidean_dist - calculates the Euclidean Distance between two points
  *
- * @param[in,out] image1 - the target image to search
+ * @param[in] point A - x,y coordinate of a point in the image
+ * @param[in] point B - x,y coordinate of a point in the image
  *
  *****************************************************************************/
-/************************************************************************
-   Function: euclidean_dist
-   Author: Zachary Pierson
-   Description: calculates the Euclidean Distance between two points
- ************************************************************************/
 double euclidean_dist(point A, point B)
 {
     return sqrt( pow(A.x - B.x, 2) + pow(A.y - B.y, 2));
@@ -67,19 +63,17 @@ double euclidean_dist(point A, point B)
  * @author Zachary Pierson
  *
  * @par Description:
- * Image_Model Constructor - Constructs Image_Model object from Image
+ * Forward Hausdorff - Returns a list of the directed hausorff distances.
+ * An ordered list with best matching distances first. This is ORDERS of
+ * magnitueds FASTER than the original hausdorff distance calculation.
+ * Uses voronoi surface to faster calculate Hausdorff values.
  *
- * @param[in,out] image1 - the target image to search
+ * @param[in] modelPoints - the list of points in the model
+ * @param[in,out] target - the Image_model object of the target image
+ *
+ * @returns vector<double> list of sorted distances
  *
  *****************************************************************************/
-/************************************************************************
-   Function: Forward Hausdorff
-   Author: Zachary Pierson
-   Description: Returns a list of the directed hausorff distances.  An ordered
-        list with best matching distances first. This is ORDERS of magnitueds
-        FASTER than the original hausdorff distance calculation. Uses voronoi
-        surface to faster calculate Hausdorff values.
- ************************************************************************/
 vector<double> forward_hausdorff(vector<point> modelPoints, Image_Model& target)
 {
     /*
@@ -122,23 +116,23 @@ vector<double> forward_hausdorff(vector<point> modelPoints, Image_Model& target)
 }
 
 /**************************************************************************//**
- * @author Zachary Pierson
+ * @author Zachary Pierson, Hannah Aker, Steven Huerta
  *
  * @par Description:
- * Image_Model Constructor - Constructs Image_Model object from Image
+ * reverse_hausdorff - Box Reverse Hausdorff. Returns a list of the
+ * directed hausorff distances.  An ordered list with best matching distances
+ * first. This is ORDERS of magnitueds FASTER than the original hausdorff
+ * distance calculation.
  *
- * @param[in,out] image1 - the target image to search
+ * @param[in] ts - tsObject containing transformation
+ * @param[in] target - the Image_model object of the target image
+ * @param[in,out] model - the Image_model object of the model image
+ *
+ * @returns vector<double> list of sorted distances
  *
  *****************************************************************************/
-/************************************************************************
-   Function: Box Reverse Hausdorff
-   Author: Zachary Pierson
-   Description: Box Reverse Hausdorff. Returns a list of the directed hausorff
-        distances.  An ordered list with best matching distances first. This is
-        ORDERS of magnitueds FASTER than the original hausdorff distance calculation.
-   TO DO: need to implement
- ************************************************************************/
-vector<double> reverse_hausdorff(tsObject ts, Image_Model& target, Image_Model& model)
+vector<double> reverse_hausdorff(tsObject ts, Image_Model& target,
+                                 Image_Model& model)
 {
     //clock_t t = clock();
     vector<double> distance;
@@ -146,29 +140,31 @@ vector<double> reverse_hausdorff(tsObject ts, Image_Model& target, Image_Model& 
     //make sure there are points to process
     if(target.points.size() == 0)
         return distance;
+
+    // create a list of points describing the shape
+    // of the transformed model, which will form the "box" from which
+    // we will pull our points
     int x1 = ts.transXCenter ;
     int x2 = x1 + model.cols ;
     int y1 = ts.transYCenter ;
     int y2 = y1 + model.rows ;
 
-
+    // for all the points in the target image
     for(  unsigned int i = 0; i < target.points.size(); i++)
-    {
-
-        /*//Continue if index outside of model space
-        if(  ( !((target.points[i].x < model.cols)&&(target.points[i].x > 0 )) ) ||
-             ( !((target.points[i].y < model.rows )&&(target.points[i].y > 0 )) ) )
+    {        
+        // for the ones that are inside our transformed "box"
+        if( (target.points[i].x >= x1)&&(target.points[i].x<x2)&&
+            (target.points[i].y>=y1)&&(target.points[i].y<y2) )
         {
-            continue;
-        }*/
-        if( (target.points[i].x >= x1)&&(target.points[i].x<x2)&&(target.points[i].y>=y1)&&(target.points[i].y<y2) )
-        {
+            // Let's find the points distance from the points on the image
+            // to the model
             int xVor = target.points[i].x - ts.transXCenter ;
             int yVor = target.points[i].y - ts.transYCenter ;
             distance.push_back( model.voronoi[xVor][yVor] );
         }
     }
 
+    // sort the list
     sort(distance.begin(), distance.end());
     //cout << "Reverse Hausdorff Distance (Voronoi): " << distance.back() << endl;
 
@@ -178,21 +174,20 @@ vector<double> reverse_hausdorff(tsObject ts, Image_Model& target, Image_Model& 
 }
 
 /**************************************************************************//**
- * @author Zachary Pierson
+ * @author Zachary Pierson, Hannah Aker, Steven Huerta
  *
  * @par Description:
- * Image_Model Constructor - Constructs Image_Model object from Image
+ * directed_hausdorff - Box Reverse Hausdorff. Returns a list of the
+ * directed hausorff distances.  An ordered list with best matching distances
+ * first. This is ORDERS of magnitueds FASTER than the original hausdorff
+ * distance calculation.
  *
- * @param[in,out] image1 - the target image to search
+ * @param[in,out] B - list of points in the model image
+ * @param[in,out] A - list of points in the target image
+ *
+ * @returns vector<double> list of sorted distances
  *
  *****************************************************************************/
-/************************************************************************
-   Function: Directed Hausdorff
-   Author: Zachary Pierson
-   Description: Hausdorff WITHOUT voronoi surface. This is ORDERS of
-        magnitudes SLOWER than using the voronoi surface as a look up
-        table for distances.
- ************************************************************************/
 vector<double> directed_hausdorff(vector<point> &B, vector<point>& A)
 {
     //clock_t t = clock();
@@ -227,6 +222,7 @@ vector<double> directed_hausdorff(vector<point> &B, vector<point>& A)
         min = 9999;
     }
 
+    // sort the distances
     sort(distance.begin(), distance.end());
     cout << "Hausdorff Distance: " << distance.back() << endl;
 
@@ -236,22 +232,28 @@ vector<double> directed_hausdorff(vector<point> &B, vector<point>& A)
 }
 
 /**************************************************************************//**
- * @author Zachary Pierson
+ * @author Hannah Aker, Zachary Pierson, Steven Huerta
  *
  * @par Description:
- * Image_Model Constructor - Constructs Image_Model object from Image
+ * decomp - Finds interesting transformation spaces by continually dividing
+ * up the transformation space, evaluating for possible matches, and
+ * dividing again until no further division is possible
  *
- * @param[in,out] image1 - the target image to search
+ * @param[in,out] target - Image_Model of the target
+ * @param[in,out] model - Image_Model of the model
+ * @param[in,out] pixelErrorThresh - pixel threshold for maximum distance
+ * @param[in,out] percentList - percent of list of distances to evaluate
+ * @param[in,out] alpha - contols skew factor - NOT IMPLEMENTED
+ *
+ * @returns vector<double> list of transformations as tsObjects
  *
  *****************************************************************************/
-/************************************************************************
-   Function: Decomp
-   Author: Hannah Aker
-   Description: Finds interesting transformation spaces
- ************************************************************************/
-vector<tsObject> decomp(Image_Model& target, Image_Model& model, float pixelErrorThresh = 1, float percentList = 1.0, int alpha = 1)
+vector<tsObject> decomp(Image_Model& target, Image_Model& model,
+                        float pixelErrorThresh = 1, float percentList = 1.0,
+                        int alpha = 1)
 {
     cout << "Decomp!" << endl;
+    // initialize a list of container for tsObjects
     vector<tsObject> realMatches;
 
     // initialize our first transformation space paramaters
@@ -300,51 +302,62 @@ vector<tsObject> decomp(Image_Model& target, Image_Model& model, float pixelErro
 
 
 /**************************************************************************//**
- * @author Zachary Pierson
+ * @author Zachary Pierson, Hannah Aker, Steven Huerta
  *
  * @par Description:
- * Image_Model Constructor - Constructs Image_Model object from Image
+ * calcGamma - Calculate Gamma for Decomp function. Gamma is the additional
+ * length to be added to the threshold so that we can evaluate for possible
+ * matches of the model.
  *
- * @param[in,out] image1 - the target image to search
+ * @param[in] ts - tsObject holding transform
+ * @param[in] xMax - holds width of model
+ * @param[in] yMax - holds height of model
+ *
+ * @returns double - gamma
  *
  *****************************************************************************/
-/************************************************************************
-   Function: calcGamma
-   Author: Steven Heurta
-   Description: Calculate Gamma for Decomp function
- ************************************************************************/
 double calcGamma( tsObject ts, int xMax, int yMax )
 {
 
     //if(ts.transXMax == ts.transXMin)
       //  printf("hahaha!\n");
-    double x = ts.transXMax - ts.transXMin + (( ts.scaleXMax - ts.scaleXMin )*xMax/2.0);
-    double y = ts.transYMax - ts.transYMin + (( ts.scaleYMax - ts.scaleYMin )*yMax/2.0);
+
+    // calculate the x and y to the furthest point in the
+    // transformation space
+    double x = ts.transXMax - ts.transXMin + (
+             ( ts.scaleXMax - ts.scaleXMin )*xMax/2.0);
+
+    double y = ts.transYMax - ts.transYMin + (
+             ( ts.scaleYMax - ts.scaleYMin )*yMax/2.0);
 
     double gamma = (x*x) + (y*y) ;
 
+    // finish calculating the euclidean distance of that distance from the
+    // origin
     gamma = sqrt( gamma ) ;
 
     return gamma;
 }
 
-
 /**************************************************************************//**
- * @author Zachary Pierson
+ * @author Hannah Aker, Zachary Pierson, Steven Huerta
  *
  * @par Description:
- * Image_Model Constructor - Constructs Image_Model object from Image
+ * isInteresting - determines if a transformation space is intersting, runs
+ * hausdorff and examines values returned.
  *
- * @param[in,out] image1 - the target image to search
+ * @param[in,out] ts - object containing the transformation space
+ * @param[in,out] target - Image_Model of the target
+ * @param[in,out] model - Image_Model of the model
+ * @param[in] percentList - percent of list of distances to evaluate
+ * @param[in] threshold - distance threshold for foward partial HD
+ *
+ * @returns true    - space contains possible matches
+ * @returns false   - no possible matches in space
  *
  *****************************************************************************/
-/************************************************************************
-   Function: isInteresting
-   Author: Steven Heurta
-   Description: determines if a transformation space is intersting, runs
-       hausdorff and examines values returned.
- ************************************************************************/
-bool isInteresting( tsObject & ts, Image_Model & target, Image_Model & model, float percentList, int threshold )
+bool isInteresting( tsObject & ts, Image_Model & target, Image_Model & model,
+                    float percentList, int threshold )
 {
 
     // transform model with quadruple
@@ -355,6 +368,8 @@ bool isInteresting( tsObject & ts, Image_Model & target, Image_Model & model, fl
     int hausIndex = percentList * (haus.size()-1);
     double hausDist = haus.at(hausIndex);
 
+    // if the distance is more than the treshold, there is no possible
+    // match in this space
     if( hausDist < threshold )
         return true ;
     else
@@ -362,24 +377,24 @@ bool isInteresting( tsObject & ts, Image_Model & target, Image_Model & model, fl
 }
 
 /**************************************************************************//**
- * @author Zachary Pierson
+ * @author Hannah Aker, Zachary Pierson, Steven Huerta
  *
  * @par Description:
- * Image_Model Constructor - Constructs Image_Model object from Image
+ * divide - divides transformation space into subspaces, and pushes them
+ * to a list to be passed back.
  *
- * @param[in,out] image1 - the target image to search
+ * @param[in] ts - object containing the transformation space
+ *
+ * @returns vector<tsObject>    - list containing transformation spaces
  *
  *****************************************************************************/
-/************************************************************************
-   Function: divide
-   Author: Steven Heurta
-   Description: divides the transformation into 4 smaller transformation
-       spaces
- ************************************************************************/
 vector<tsObject> divide( tsObject ts )
 {
+    // instantiate list for storing subspaces
     vector<tsObject> subTransformSpace ;
 
+    // create subspaces from transformation space
+    // SCALING NOT IMPLEMENTED, SO ONLY 4 SUBSPACES NEEDED & NOT 16
     tsObject cell_1_1( ts.transXMin , ts.transXCenter,
                      ts.transYMin , ts.transYCenter,
                      ts.scaleXMin , ts.scaleXMax,
@@ -448,6 +463,7 @@ vector<tsObject> divide( tsObject ts )
 //                     ts.scaleXCenter , ts.scaleXMax,
 //                     ts.scaleYCenter , ts.scaleYMax) ;
 
+    // push subspaces into list
     subTransformSpace.push_back( cell_1_1 ) ;
 //    subTransformSpace.push_back( cell_1_2 ) ;
 //    subTransformSpace.push_back( cell_1_3 ) ;
@@ -472,12 +488,15 @@ vector<tsObject> divide( tsObject ts )
 }
 
 /**************************************************************************//**
- * @author Zachary Pierson
+ * @author Hannah Aker, Zachary Pierson, Steven Huerta
  *
  * @par Description:
- * Image_Model Constructor - Constructs Image_Model object from Image
+ * transform - transforms list of points of the model
  *
- * @param[in,out] image1 - the target image to search
+ * @param[in] ts - object containing the transformation space
+ * @param[in,out] transImage - object containing the transformation space
+ *
+ * @returns vector<point> - vector of transformed points
  *
  *****************************************************************************/
 /************************************************************************
--- imagemodel.cpp
+++ imagemodel.cpp
@@ -301,7 +301,7 @@ int Image_Model::push_neighbors(queue<point> &q, point p)
  *****************************************************************************/
 bool Image_Model::match(Image& img)
 {
-    // resclase
+    // rescales image
     rescale( image, img.Height(), img.Width() );
 
     return true;
